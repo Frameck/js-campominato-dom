bdbc55cc8c98eb62458f9347ca27e9c1797bdeee
Frameck <fra9879@gmail.com>
2021-10-20 17:14:53 +0200

    Completed exercise - bombs and scoring system

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ const playButton = document.getElementById('play-button')
 const boxContainer = document.querySelector('.box-container')
 const difficultySelect = document.getElementById('difficulty-select')
 
-const bombArray = []
+const bombArray = [] // tiene traccia della posizione delle bombe
+const userPoints = [] // tiene traccia dei punti segnati dallo user
 
 
-playButton.addEventListener('click', () => {
+playButton.addEventListener('click', function() {
     // al momento del click leggo il livello di difficoltà inserito dall'utente
     const difficultyLevel = difficultySelect.value
 
@@ -42,57 +43,74 @@ function generateGridLayout(difficultyLevel) {
             break;
     }
 
+    // genero le bombe
+    const totBombNumber = 16
+    generateBombs(totBombNumber, totalBoxNumber)
+
     // calcolo le dimensioni del box in base al 'totalBoxNumber'
     const boxWidth = 100 / Math.sqrt(totalBoxNumber)
     
     // creo ed inserisco i box nel DOM
     for (let i = 1; i < totalBoxNumber + 1; i++) {
+        // genero i box element
         const box = document.createElement('div')
         box.classList.add('box')
         box.textContent = i
         box.style.width = boxWidth + '%'
         box.style.height = boxWidth + '%'
 
+
         box.addEventListener('click', function() {
-            this.classList.toggle("box-active")
-            console.log(this, 'NORMALE')
+            const clickedBoxIndex = parseInt(this.textContent)
+            const boxIsBomb = bombArray.includes(clickedBoxIndex)
+
+            // controllo se l'indice del box corrisponde ad una bomba
+            // se TRUE aggiungo la classe bomb e il gioco termina (calcolo i punti che l'utente ha fatto)
+            // se FALSE il box si colora e il gioco continua
+            if (boxIsBomb) {
+                this.classList.add('bomb')
+                let totUserPoints = userPoints.length
+                // aggiungo 1,5sec di delay prima di resettare il gioco
+                setTimeout(function() {
+                    alert(`Hai trovato una bomba.\nHai perso con un punteggio di ${totUserPoints}`)
+                    boxContainer.innerHTML = ''
+                }, 1000);
+            } else {
+                this.classList.add('box-active')
+                userPoints.push(clickedBoxIndex)
+            }
+            console.log(this.className)
+            console.log(userPoints)
         })
         
         boxContainer.appendChild(box)
     }
-    
-    // tentativo di aggiungere le bombe
-    const numBombe = 16
-
-    // finchè l'array non è uguale a 16 continuo a pushare elementi
-    for (let i = 0; i < numBombe; i++) {
-        const generatedRandomBomb = randomIntFromInterval(1, totalBoxNumber)
-        const bombAlreadyExists = bombArray.includes(generatedRandomBomb)
-
-        if (bombAlreadyExists) {
-            i--
-        } else {
-            bombArray.push(generatedRandomBomb)
-        }
-    }
-    
-    // for (let j = 0; j < bombArray.length; j++) {
-    //     const generatedRandomBomb = bombArray[j];
-    //     const boxElement = document.querySelector(`:nth-child(${generatedRandomBomb})`)
-    //     boxElement.addEventListener('click', function () {
-    //         this.classList.remove('box-active')
-    //         this.classList.add('bomb')
-    //         alert('Hai trovato una bomba!\nHai perso')
-    //         console.log(this, 'BOMBA')
-    //     })
-    // }
 }
 
-console.log(bombArray);
-
+console.log(bombArray)
 
 
 // genera un numero casuale tra un min e un max (included)
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+/**
+ * Funzione che genera un numero di bombe passato come argomento
+ * @param {number} totBombNumber numero di bombe da generare
+ * @param {number} totalBoxNumber numero totale di box = 49 || 81 || 100
+ */
+function generateBombs(totBombNumber, totalBoxNumber) {
+    // finchè l'array non contiene 16 numeri unici continuo a pushare elementi
+    for (let i = 0; i < totBombNumber; i++) {
+        const newBomb = randomIntFromInterval(1, totalBoxNumber)
+        const bombAlreadyExists = bombArray.includes(newBomb)
+        
+        if (bombAlreadyExists) {
+            i--
+        } else {
+            bombArray.push(newBomb)
+        }
+    }
 }
